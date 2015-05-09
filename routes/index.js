ae088ad8e8c85bc78b586d9bf732f6042432cef3
Richard Hosking <r.hosking@auckland.ac.nz>
2015-05-09 17:59:13 +1200

    testing zmq

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var request = require('request')
 var router = express.Router();
 
+// subber.js 
+var zmq = require('zmq')
+  , sock = zmq.socket('sub');
+ 
+sock.connect('tcp://127.0.0.1:3000');
+sock.subscribe('kitty cats');
+console.log('Subscriber connected to port 3000');
+ 
+sock.on('message', function(topic, message) {
+  console.log('received a message related to:', topic, 'containing message:', message);
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
