c59c88621c08d8e55d37a66247cb22f4ec76d312
Richard Hosking <r.hosking@auckland.ac.nz>
2015-05-09 14:29:10 +1200

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,56 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* Control Flow Routes */
+
+router.post('/control/initialize', function(req, res, next) {
+  res.send('Initialized');
+});
+
+router.post('/control/startExperiment', function(req, res, next) {
+  res.send('Experiment Started');
+});
+
+router.post('/control/startSession', function(req, res, next) {
+  res.send('Session Started');
+});
+
+router.post('/control/endSession', function(req, res, next) {
+  res.send('Session Ended');
+});
+
+
+/* Add New Items */
+
+router.post('/addNew/bird', function(req, res, next) {
+  res.send('Bird Added');
+});
+
+router.post('/addNew/stageDefinition', function(req, res, next) {
+  res.send('Stage  Definition Added');
+});
+
+
+/* Freeform Mode Commands */
+
+router.post('/freeForm/dropMeat', function(req, res, next) {
+  res.send('Meat Dropped');
+});
+
+router.post('/freeForm/lightOn', function(req, res, next) {
+  res.send('Light Turned On');
+});
+
+router.post('/freeForm/lightsOff', function(req, res, next) {
+  res.send('Lights turned Off');
+});
+
+
+/* Data Retrieval Methods */
+
+router.post('/data/getBirds', function(req, res, next) {
+  res.send('Bird Data');
+});
+
 module.exports = router;
