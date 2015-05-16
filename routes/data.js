4549576f8628adab83bfbc58ed3112e9c31944a1
Richard Hosking <r.hosking@auckland.ac.nz>
2015-05-16 14:57:01 +1200

    integrating mongoskin

--- routes/data.js
+++ routes/data.js
@@ -4,7 +4,7 @@ var exports = module.exports = {};
 
 /* Get methods */
 
-exports.logTrial = function{} {
+exports.logTrial = function() {
 	db.collection('trials').insert({trialID: "Guns N' Roses", result: "fail" }, function(err, result) {
     	if (err) throw err;
     	if (result) console.log('Added!');
@@ -12,25 +12,25 @@ exports.logTrial = function{} {
 
 }
 
-exports.newBird = function{} {
+exports.newBird = function() {
 
 }
 
-exports.newStage = function{} {
+exports.newStage = function() {
 
 }
 
-exports.setDeviceMapping = function{} {
+exports.setDeviceMapping = function() {
 
 }
 
 
 /* Setter methods */
 
-exports.getDeviceMapping = function{} {
+exports.getDeviceMapping = function() {
 
 }
 
-exports.getTrials = function{} {
+exports.getTrials = function() {
 
 }
