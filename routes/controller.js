6b465447dd43b1cd22d7701756a4646d96076f23
Richard Hosking <r.hosking@auckland.ac.nz>
2015-05-17 20:56:36 +1200

    added meatdropper method

--- routes/controller.js
+++ routes/controller.js
@@ -169,7 +169,7 @@ var experiment = new machina.Fsm( {
     },
     // External Facing API
 
-    initialize: function() {
+    initialize2: function() {
         console.log("initialize API 2");
         this.handle( "initialize" );
     },
