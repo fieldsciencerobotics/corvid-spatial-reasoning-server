73708ddf163d6ab778f3a82d3be6443b2786890d
Richard Hosking <r.hosking@auckland.ac.nz>
2015-06-06 18:17:21 +1200

    testing parsing lagarto messages

--- routes/devices.js
+++ routes/devices.js
@@ -16,10 +16,8 @@ var request = require('request');
 // Not to be confussed with the experimental node ID, which will be unique to the arrangement of each session
 
 // Feeder Devices
-var feederNameToLagartoID = [{'name': 'a', 'lagartoID': '7'}, {'name': 'b', 'lagartoID': '0'}, {'name': 'c', 'lagartoID': '0'}, 
-						{'name': 'd', 'lagartoID': '0'}, {'name': 'e', 'lagartoID': '0'}, {'name': 'f', 'lagartoID': '0'}, 
-						{'name': 'g', 'lagartoID': '0'}, {'name': 'h', 'lagartoID': '0'}, {'name': 'i', 'lagartoID': '0'}, 
-						{'name': 'j', 'lagartoID': '0'}, ];
+var feederNameToLagartoID = {'a': '7', 'b': '0', 'c': '0', 'd': '0', 'e': '0',
+							'f': '0', 'g': '0', 'h': '0', 'i': '0', 'j': '0'};
 var feederFunctionToLagartoDotReference = {'dropMeat': '11.0'}; // battery, reset etc etc etc
 
 // Indicator device
@@ -41,94 +39,100 @@ var Meerkat = function() {
 	console.log('Subscriber connected to port 5001');
 	 
 	sock.on('message', function(topic, message) {
-	  console.log('received a message related to:', topic, 'containing message:', message);
+	  	console.log('received a message related to:', topic, 'containing message:', message);
 
-	  // all the parsing work should take place here
 
-	  // Reset
-	  // "{"lagarto": 
-	  		//{
-	  		//"status": 
-	  			//[{"direction": "out", "name": "Reset_7", "timestamp": "28 May 2015 06:00:17", "value": "on", "location": "SWAP", "type": "bin", "id": "7.12.0"}], 
-	  		//"procname": "Lagarto-SWAP", 
-	  		//"httpserver": "10.1.1.6:8001"
-	  		//}
-	  	//}"
+	  	console.log(message['lagarto']['status']);
 
-	  // Perch off
-	  //"{"lagarto": 
+	  	for (var i=0; i<arr.length; i++)
+	        if (arr[i] === val)                    
+	            return i;
+	    return false;
+
+		// all the parsing work should take place here
+
+		// Reset
+		// "{"lagarto": 
+				//{
+				//"status": 
+					//[{"direction": "out", "name": "Reset_7", "timestamp": "28 May 2015 06:00:17", "value": "on", "location": "SWAP", "type": "bin", "id": "7.12.0"}], 
+				//"procname": "Lagarto-SWAP", 
+				//"httpserver": "10.1.1.6:8001"
+				//}
+			//}"
+
+		// Perch off
+		//"{"lagarto": 
 			//{"status": 
 				//[{"direction": "inp", "name": "Perch_triggered_7", "timestamp": "28 May 2015 05:59:34", "value": "off", "location": "SWAP", "type": "bin", "id": "7.17.0"}], 
 			//"procname": "Lagarto-SWAP", "httpserver": "10.1.1.6:8001"}}"
 
-	  // Perch event to parse
-	  //"{"lagarto": 
+		// Perch event to parse
+		//"{"lagarto": 
 			//{"status": 
 				//[{"direction": "inp", "name": "Perch_triggered_7", "timestamp": "28 May 2015 05:59:34", "value": "on", "location": "SWAP", "type": "bin", "id": "7.17.0"}], 
 			//"procname": "Lagarto-SWAP", "httpserver": "10.1.1.6:8001"}}"
 
 
-	  //Get the event name
-	  //message.lagarto.status [get the array element].name
+		//Get the event name
+		//message.lagarto.status [get the array element].name
 
-	  //Get the event lagarto dot prefix
-	  //message.lagarto.status [get the array element].id
+		//Get the event lagarto dot prefix
+		//message.lagarto.status [get the array element].id
 
-	  //Get the event value
-	  //message.lagarto.status [get the array element].value
+		//Get the event value
+		//message.lagarto.status [get the array element].value
 
-	  // Event Type
-	  eventType = 'perchEvent2'; // or meatDropped, or battery, or light or etc...
-	  deviceID = 1; // get this from
-	  perchID = 1;
-	  value = 'on';
-
-
-	  switch (eventType) {
-	    case 'perchEvent':
-	        console.log("PerchEvent", deviceID, value);
-
-	        // Pasrse message, and topic
-
-	        perchID = 1; //set this based on the contents of the message
-	        self.emit('perchEvent', perchID);
+		// Event Type
+	  	eventType = 'perchEvent2'; // or meatDropped, or battery, or light or etc...
+	  	deviceID = 1; // get this from
+	  	perchID = 1;
+	  	value = 'on';
 
 
-	        break; 
-	    case 'MeatFinishedDropping':
-	        console.log("MeatFinishedDropping", deviceID, value);
+	  	switch (eventType) {
+	    	case 'perchEvent':
+	        	console.log("PerchEvent", deviceID, value);
 
-	        // Pasrse message, and topic
+	        	// Pasrse message, and topic
 
-	        feederID = 1; //set this based on the contents of the message
-	        self.emit('MeatDropped', feederID)
-
-	        break;
-	    case 'LightChanged':
-	        console.log("LightChanged", lightID);
-
-	        // Pasrse message, and topic
-
-	        lightID = 1; //set this based on the contents of the message
-	        self.emit('LightChanged', lightID)
-
-	        break;
-	    case 'BatteryUpdate':
-	        console.log("BatteryUpdated", deviceID);
-
-	        // Pasrse message, and topic
-
-	        lightID = 1; //set this based on the contents of the message
-	        self.emit('BatteryUpdated', deviceID)
-
-	        break; 
-	    default: 
-	        console.log("unhandled event");
-
-	        perchID = 1; //set this based on the contents of the message
-	        self.emit('dunno', perchID);
+	        	perchID = 1; //set this based on the contents of the message
+	        	self.emit('perchEvent', perchID);
 
 
+	        	break; 
+	    	case 'MeatFinishedDropping':
+		        console.log("MeatFinishedDropping", deviceID, value);
+
+		        // Pasrse message, and topic
+
+		        feederID = 1; //set this based on the contents of the message
+		        self.emit('MeatDropped', feederID)
+
+	        	break;
+	    	case 'LightChanged':
+		        console.log("LightChanged", lightID);
+
+		        // Pasrse message, and topic
+
+		        lightID = 1; //set this based on the contents of the message
+		        self.emit('LightChanged', lightID)
+
+		        break;
+	    	case 'BatteryUpdate':
+		        console.log("BatteryUpdated", deviceID);
+
+		        // Pasrse message, and topic
+
+		        lightID = 1; //set this based on the contents of the message
+		        self.emit('BatteryUpdated', deviceID)
+
+		        break; 
+	    	default: 
+		        console.log("unhandled event");
+
+		        perchID = 1; //set this based on the contents of the message
+		        self.emit('dunno', perchID);
 		}
 	});
 
@@ -178,7 +182,7 @@ exports.getDevices = function() {
 exports.dropMeat = function(deviceName) {
 
 	// map device name, and the function to below
-	deviceID = '7'; //feederNameToLagartoID -> get the item which matches deviceName, and pull the lagartoID
+	deviceID = feederNameToLagartoID[deviceName];
 	functionID = feederFunctionToLagartoDotReference.dropMeat; //maps to 11.0
 	deviceAndFunctionID = deviceID + '.' + functionID;
 	value = 'true';
